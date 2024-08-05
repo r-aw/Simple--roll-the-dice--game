b6293feb6d388ba8a3296afc6d7c89e68936ef5e
Igor Rocha <igoreidji92@hotmail.com>
2024-08-05 22:25:17 +0100

    commit

--- rollthedice.py
+++ rollthedice.py
@@ -1,13 +1,18 @@
+#for GUI use tkinter
 #import numpy library
+#if using numpy library first number is inlcusive and last number exclusive
+#if using random library first and last number are inclusive
 
 import numpy as np
 
+#roll dice message and random number play
 
 msg = print('Roll the dice!')
 number = np.random.randint(1,6)
 
 print(number)
 
+# if statement to display dice result
 
 if number == 3:
     print('You are the winner. Play again!')
